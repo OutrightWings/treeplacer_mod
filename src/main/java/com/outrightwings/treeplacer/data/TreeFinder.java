8c6d2212a550b160c2fbb61cd3ccc440e55fd279
lily <46290459+OutrightWings@users.noreply.github.com>
2023-03-15 02:03:04 -0700

    Mixin Setup

---
+++ src/main/java/com/outrightwings/treeplacer/data/TreeFinder.java
@@ -0,0 +1,28 @@
+package com.outrightwings.treeplacer.data;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class TreeFinder {
+
+    public static void GetBiomeBasedTreeFeature(ServerLevel level, BlockState state, BlockPos pos){
+        ResourceLocation saplingName = ForgeRegistries.BLOCKS.getKey(state.getBlock());
+        ResourceLocation biomeName = getBiome(level.getBiome(pos));
+        //Debug Print
+        System.out.println(saplingName.toString() + " " + biomeName.toString());
+    }
+
+    //Stole and modified DebugScreen's method
+    private static ResourceLocation getBiome(Holder<Biome> biome) {
+        return biome.unwrap().map((resourceKey) -> {
+            return resourceKey.location();
+        }, (biome1) -> {
+            return null;
+        });
+    }
+}
---
+++ src/main/java/com/outrightwings/treeplacer/mixin/AbstractTreeGrowerMixin.java
@@ -0,0 +1,28 @@
+package com.outrightwings.treeplacer.mixin;
+
+import com.outrightwings.treeplacer.data.TreeFinder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.grower.AbstractTreeGrower;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+@Mixin(AbstractTreeGrower.class)
+public class AbstractTreeGrowerMixin {
+
+    @Inject(at = @At("HEAD"), method = "growTree(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/util/RandomSource;)Z")
+    void growTree(ServerLevel level, ChunkGenerator chunkGenerator, BlockPos pos, BlockState state, RandomSource random, CallbackInfoReturnable<Boolean> cir){
+        TreeFinder.GetBiomeBasedTreeFeature(level,state,pos);
+    }
+}
