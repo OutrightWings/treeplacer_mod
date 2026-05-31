2a778dae659accf8561ae93eba956e3de15b0777
OutrightWings <lilymason@msn.com>
2026-05-31 10:24:35 -0700

    List overrides command

--- neoforge/src/main/java/com/outrightwings/NeoForgeMain.java
+++ neoforge/src/main/java/com/outrightwings/NeoForgeMain.java
@@ -1,13 +1,13 @@
 package com.outrightwings;
 
+import com.outrightwings.command.ListCommand;
 import com.outrightwings.data.MegaTreeDataReloadListener;
 import com.outrightwings.data.SingleTreeDataReloadListener;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
+import net.neoforged.neoforge.event.AddReloadListenerEvent;
 
 @Mod(Constants.MOD_ID)
 public class NeoForgeMain {
@@ -21,5 +21,9 @@ public class NeoForgeMain {
     public void onResourceReload(final AddServerReloadListenersEvent event){
         event.addListener(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID,"single_tree_data"),new SingleTreeDataReloadListener());
         event.addListener(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID,"mega_tree_data"),new MegaTreeDataReloadListener());
+    }
+    @SubscribeEvent
+    private void registerCommands(RegisterCommandsEvent event) {
+        ListCommand.register(event.getDispatcher());
     }
 }
