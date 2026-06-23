ad918dbfec3c84925891ffa9fad0db346e418d87
OutrightWings <lilymason@msn.com>
2026-06-23 15:45:04 -0700

    1.21.5 - 1.21.9

--- neoforge/src/main/java/com/outrightwings/platform/NeoForgePlatformHelper.java
+++ neoforge/src/main/java/com/outrightwings/platform/NeoForgePlatformHelper.java
@@ -21,6 +21,6 @@ public class NeoForgePlatformHelper implements IPlatformHelper {
     @Override
     public boolean isDevelopmentEnvironment() {
 
-        return !FMLLoader.getCurrent().isProduction();
+        return !FMLLoader.isProduction();
     }
 }
