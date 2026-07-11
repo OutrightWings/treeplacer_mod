e52f0757132035e1f52c9fb85218783d60f83523
OutrightWings <lilymason@msn.com>
2026-07-11 15:24:34 -0700

    version specific changes

--- common/src/main/java/com/outrightwings/mixin/BushBlockMixin.java
+++ common/src/main/java/com/outrightwings/mixin/BushBlockMixin.java
@@ -11,19 +11,20 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.VegetationBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BushBlock.class)
+@Mixin(VegetationBlock.class)
 public class BushBlockMixin extends Block{
     public BushBlockMixin(Properties properties) {
         super(properties);
     }
 
-    @Inject(at = @At(value = "HEAD"), method = "mayPlaceOn(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z",cancellable = true)
+    @Inject(at = @At(value = "HEAD"), method = "mayPlaceOn",cancellable = true)
     void mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         if (level instanceof Level real) {
             //Get tag
@@ -33,8 +34,8 @@ public class BushBlockMixin extends Block{
             if (blockTagLocation != null) {
                 //Check if tag was defined
                 TagKey<Block> blockTag = TagKey.create(Registries.BLOCK, blockTagLocation);
-                Registry<Block> registry = real.registryAccess().registryOrThrow(Registries.BLOCK);
-                if (registry.getTag(blockTag).isPresent()) {
+                Registry<Block> registry = real.registryAccess().lookupOrThrow(Registries.BLOCK);
+                if (registry.getTags().anyMatch(pair -> pair.key().equals(blockTag))) {
                     cir.setReturnValue(state.is(blockTag));
                 }
             }
